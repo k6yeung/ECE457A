75258c9afc350d033420777571afd7ee1531df24
Kyle Yeung <k6yeung@uwaterloo.ca>
2018-07-14 13:02:05 -0400

    decode particle position

--- pso/pso.py
+++ pso/pso.py
@@ -10,28 +10,31 @@ def accuracy(args):
     result = np.ones(len(args), float)
     i = 0
     for pos in args:    
-        # Retrieve dataset row encoding of position
+        # Retrieve dataset row encoding from particle pos
         out = 0
-        for bit in bitlist:
+        for bit in pos:
             out = (out << 1) | bit
-        if pos <= 2 :
-            result[i] = 100.0-df.iloc[out]
+        # Return 1.0 - accuracy since PSO optimizes for minima
+        if out < 24 :
+            result[i] = 1.0-df.iloc[out].accuracy
         else :
-            result[i] = 100.0
+            result[i] = 1.0
         i = i+1
-    # print(result)    
     return result
 
 
 def main():
-    # print(df.iloc[2])
     # Set-up hyperparameters
-    options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+    options = {'c1': 0.5, 'c2': 0.3, 'w':0.9, 'k': 2, 'p': 2}
 
     optimizer = ps.discrete.binary.BinaryPSO(n_particles=2, dimensions=5, options=options)
+    
     # Perform optimization 
-    cost, pos = optimizer.optimize(accuracy, print_step=100, iters=100, verbose=4)
+    cost, pos = optimizer.optimize(accuracy, print_step=100, iters=1000, verbose=4)
 
+    print("Optimal Particle Position: " + str(pos))
+    print("Optimal Accuracy: " + str(1.0-cost))
+    
     
 
 if __name__ == "__main__":
